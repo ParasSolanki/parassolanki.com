a9795f38f2b1f8f974c34a72c7a531a11b181d95
ParasSolanki <parassolanki151@gmail.com>
2024-12-11 21:50:20 +0530

    fix: lint issue updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,9 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 // import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
 
-import tsParser from '@typescript-eslint/parser';
-
 const config = [
   ...eslintPluginAstro.configs.recommended,
   // ...eslintPluginJsxA11y.configs.recommended,
-  {
-    files: ['**/*.astro', '**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-    },
-    rules: {}
-  }
 ];
 
 export default config;
